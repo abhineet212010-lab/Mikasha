299ca560b84b70d756115578d3d96f58a30c7b8e
abhineet212010-lab <abhineet.21.2010@gmail.com>
2026-08-02 20:23:41 +0530

    Delete models/AutoRoles.js

--- models/AutoRoles.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const autoRoleSchema = new Schema({
-  serverId: { type: String, required: true, unique: true },
-  roleIds: [{ type: String, required: true }],
-});
-
-module.exports = model('AutoRole', autoRoleSchema);

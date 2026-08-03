95586ae8e691182f0d39342a08a2066709bf618f
abhineet212010-lab <abhineet.21.2010@gmail.com>
2026-08-03 18:34:17 +0530

    Replace chalk with ANSI color helpers to avoid ERR_REQUIRE_ESM in CommonJS runtime

--- utils/logger.js
+++ utils/logger.js
@@ -1,29 +1,37 @@
-const chalk = require('chalk');
-
 function time() {
   return new Date().toISOString();
 }
 
+// Minimal ANSI color helpers (compatible with CommonJS and no external deps)
+const colors = {
+  dim: (s) => `\x1b[2m${s}\x1b[22m`,
+  blue: (s) => `\x1b[34m${s}\x1b[39m`,
+  yellow: (s) => `\x1b[33m${s}\x1b[39m`,
+  red: (s) => `\x1b[31m${s}\x1b[39m`,
+  green: (s) => `\x1b[32m${s}\x1b[39m`,
+  magenta: (s) => `\x1b[35m${s}\x1b[39m`
+};
+
 const logger = {
   info: (...args) => {
-    console.log(`${chalk.dim(time())} ${chalk.blue('[INFO]')} ${args.join(' ')}`);
+    console.log(`${colors.dim(time())} ${colors.blue('[INFO]')} ${args.join(' ')}`);
   },
 
   warn: (...args) => {
-    console.warn(`${chalk.dim(time())} ${chalk.yellow('[WARN]')} ${args.join(' ')}`);
+    console.warn(`${colors.dim(time())} ${colors.yellow('[WARN]')} ${args.join(' ')}`);
   },
 
   error: (...args) => {
-    console.error(`${chalk.dim(time())} ${chalk.red('[ERROR]')} ${args.join(' ')}`);
+    console.error(`${colors.dim(time())} ${colors.red('[ERROR]')} ${args.join(' ')}`);
   },
 
   success: (...args) => {
-    console.log(`${chalk.dim(time())} ${chalk.green('[SUCCESS]')} ${args.join(' ')}`);
+    console.log(`${colors.dim(time())} ${colors.green('[SUCCESS]')} ${args.join(' ')}`);
   },
 
   debug: (...args) => {
     if (process.env.DEBUG) {
-      console.log(`${chalk.dim(time())} ${chalk.magenta('[DEBUG]')} ${args.join(' ')}`);
+      console.log(`${colors.dim(time())} ${colors.magenta('[DEBUG]')} ${args.join(' ')}`);
     }
   }
 };

7822202c86a2419904e5ec3f068970a34c5e9cef
abhineet212010-lab <abhineet.21.2010@gmail.com>
2026-08-03 16:27:59 +0530

    Create setup.js

---
+++ commands/logs/setup.js
@@ -0,0 +1,55 @@
+const {
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+    EmbedBuilder
+} = require("discord.js");
+
+const Guild = require("../../models/Guild");
+
+module.exports = {
+    name: "logs-setup",
+
+    data: new SlashCommandBuilder()
+        .setName("logs-setup")
+        .setDescription("Setup the server logs channel.")
+        .addChannelOption(option =>
+            option
+                .setName("channel")
+                .setDescription("Logs channel")
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    async execute(interaction) {
+
+        const channel = interaction.options.getChannel("channel");
+
+        let data = await Guild.findOne({
+            guildId: interaction.guild.id
+        });
+
+        if (!data) {
+            data = await Guild.create({
+                guildId: interaction.guild.id
+            });
+        }
+
+        data.logChannel = channel.id;
+        await data.save();
+
+        const embed = new EmbedBuilder()
+            .setColor("#5865F2")
+            .setTitle("📜 Logs Configured")
+            .setDescription(`Logs channel has been set to ${channel}.`)
+            .addFields({
+                name: "Configured By",
+                value: interaction.user.tag
+            })
+            .setTimestamp();
+
+        return interaction.reply({
+            embeds: [embed],
+            ephemeral: true
+        });
+    }
+};
